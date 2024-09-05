4b74268fa19a4a3a26f8beabc5aba7c6967cc2cf
emto0103 <emto0103@gmail.com>
2024-09-05 15:51:29 +0900

    React 4장.....

--- src/chp04/ConfirmDialog.jsx
+++ src/chp04/ConfirmDialog.jsx
@@ -4,9 +4,9 @@ function ConfirmDialog(props){
     return (
         <div>
             <p>내용을 확인하셨으면 확인 버튼을 눌러주세요.</p>
-            <button color='green'>확인</button>
-            <button1 color='green'>종료</button1>
-            <button2 color='green'>시작</button2>
+            <button className='btn1'>확인</button>
+            <button className='btn2'>확인</button>
+            <button className='btn3'>확인</button>
         </div>
     )
 }
