2cce695df9cde4b054cd55be55ee3cfb7a681048
emto0103 <emto0103@gmail.com>
2024-09-26 15:58:51 +0900

    7장

---
+++ src/chp06/Notification.jsx
@@ -0,0 +1,36 @@
+import React from "recat";
+
+const styles = {
+    wrapper: {
+        margin: 8,
+        padding: 8,
+        display: "flex",
+        flextDirection: "row",
+        border: "1px solid grey",
+        borderRadius: 16,
+    },
+    messageText: {
+        color: "black",
+        fontSize: 16,
+    },
+};
+
+class Notification extends React.Comment {
+    constructor(props){
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div style={styles.wrapper}>
+                <span style={styles.messageText}>
+                    {this.props.message}
+                </span>
+            </div>
+        );
+    }
+}
+
+export default Notification;
