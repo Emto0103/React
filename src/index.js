684497df9f1e4abf8a09ac65408a1e1536dd7608
emto0103 <emto0103@gmail.com>
2024-11-28 16:28:59 +0900

    12장

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import RequestForm from "./chp11/RequestForm";
-import NameRequestForm from "./chp11/NameRequestForm";
-import SignUp from "./chp11/SignUp";
+import Calculator3 from "./chp12/Calculator3";
+import DistanceConverter from "./chp12/distance/DistanceConverter";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <SignUp/>
+        <DistanceConverter/>
     </React.StrictMode>
 );
 //
@@ -25,4 +25,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
